bc6f761765a018e231ef74e690db5243c1f09abb
Aman Karmani <aman@tmm1.net>
2024-08-26 20:58:06 -0700

    pull out the version into a struct

--- src/v8_spy.rs
+++ src/v8_spy.rs
@@ -4,9 +4,17 @@ use spytools::ProcessInfo;
 
 use remoteprocess::{Pid, Process, ProcessMemory};
 
+struct Version {
+    major: u32,
+    minor: u32,
+    build: u32,
+    patch: u32,
+}
+
 pub struct V8Spy {
     pub pid: Pid,
     pub process: Process,
+    pub version: Version,
 }
 
 impl V8Spy {
@@ -22,21 +30,29 @@ impl V8Spy {
         #[cfg(target_os = "freebsd")]
         let _lock = process.lock();
 
-        get_v8_version(&process_info, &process);
+        let version = get_v8_version(&process_info, &process);
+        println!("v8 version: {}.{}.{}.{}", version.major, version.minor, version.build, version.patch);
 
-        Ok(Self { pid, process })
+        Ok(Self { pid, process, version })
     }
 }
 
-fn get_v8_version(process_info: &ProcessInfo, process: &Process) {
-    for ver in ["major", "minor", "build", "patch"] {
+fn get_v8_version(process_info: &ProcessInfo, process: &Process) -> Version {
+    let mut version = [0u32; 4];
+    for (i, ver) in ["major", "minor", "build", "patch"].iter().enumerate() {
         let symbol = format!("_ZN2v88internal7Version6{}_E", ver);
         let symbol = process_info.get_symbol(symbol.as_str()).unwrap();
         let mut buf = [0u8; 4];
         if let Ok(()) = process.read(*symbol as usize, &mut buf) {
-            println!("v8.{}: {:?}", ver, buf);
+            version[i] = buf[0] as u32 | (buf[1] as u32) << 8 | (buf[2] as u32) << 16 | (buf[3] as u32) << 24;
         } else {
             println!("Failed to read memory for symbol {}", ver);
         }
+    }
+    Version {
+        major: version[0],
+        minor: version[1],
+        build: version[2],
+        patch: version[3],
     }
 }
